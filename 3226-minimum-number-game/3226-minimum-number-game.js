bb8669c2124a94510d44d5c094c758c973fab223
MUHAMMED NIZAMUDHEEN <mnizamudheen81@gmail.com>
2025-04-11 15:59:39 +0530

    Time: 2 ms (88.69%), Space: 57.5 MB (39.95%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.js
+++ 3226-minimum-number-game/3226-minimum-number-game.js
@@ -4,7 +4,7 @@
  */
 var numberGame = function(nums) {
     let result=[]
-    let arr=nums.sort((a,b)=>a-b)
+    nums.sort((a,b)=>a-b)
       for (let i = 0; i < nums.length; i += 2) {
         result.push(nums[i + 1]); 
         result.push(nums[i]);    
