040345c40e8346461fafe51489ba587024cfd8e0
MUHAMMED NIZAMUDHEEN <mnizamudheen81@gmail.com>
2025-07-02 19:55:26 +0530

    Time: 7 ms (11.62%), Space: 64.1 MB (94.49%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+      let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else {
+            let profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+
+    return maxProfit;
+};
