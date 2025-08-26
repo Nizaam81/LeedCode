2e6296c8c274dfbe3b8ffb70f1f67bc0a764b8e6
MUHAMMED NIZAMUDHEEN <mnizamudheen81@gmail.com>
2025-08-26 14:31:32 +0530

    Time: 3 ms (12.39%), Space: 57.1 MB (49.3%) - LeetHub

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/3846-minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+ let sum = nums.reduce((acc,val)=>acc+val,0)
+ return sum%k ;
+ 
+};
