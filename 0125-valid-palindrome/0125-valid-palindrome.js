ee6b567420cdef48d2b7c86268879497c7e65539
MUHAMMED NIZAMUDHEEN <mnizamudheen81@gmail.com>
2025-12-06 06:02:12 +0530

    Time: 7 ms (35.32%), Space: 58 MB (56.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-     s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+ s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
    return s === s.split('').reverse().join('');
 };
